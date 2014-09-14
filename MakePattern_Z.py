c6a9dca660b76c585b944453450887c0dfcc6882
Pabitra Kumar Pati <PabitraPati@users.noreply.github.com>
2014-09-14 13:16:51 +0530

    Update MakePattern_Z.py

--- MakePattern_Z.py
+++ MakePattern_Z.py
@@ -6,16 +6,16 @@ def print_Z_pattern(line_number):
     count = number
 
     while count>0:
+    	print '\n'
         if count == number or count == 1:
             # For last line, print only stars 
             print number*'*'
-	    else:
-	        # In other lines print star only the respective position
-	        # E.g :- if line number is 3 then it will print the * at 3rd position and so on...
+	else:
+	    # In other lines print star only the respective position
+	    # E.g :- if line number is 3 then it will print the * at 3rd position and so on...
             op = (count-1)*(' ')
             op += '*'
             print op
 	count -= 1
-	
 
 print_Z_pattern(int(raw_input("Enter number of lines for the Z pattern ")))
